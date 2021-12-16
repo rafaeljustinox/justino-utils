72e78425d1b3a01b5fa0ef935b2a0880b7d3d3c3
Rafael Justino <rafael01justino@gmail.com>
2021-12-16 13:09:35 -0300

    Fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,2 @@
+0.0.2   First version
 0.0.1   First version
--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,9 @@
-import ArrayUtil from './utils/array_util'
-import bindClassMethods from './utils/class_util'
-import CronUtil from './utils/cron_util'
-import DateUtil from './utils/date_util'
-import Logger from './utils/logger'
-import StringUtil from './utils/string_util'
+import ArrayUtil from './array_util'
+import bindClassMethods from './class_util'
+import CronUtil from './cron_util'
+import DateUtil from './date_util'
+import Logger from './logger'
+import StringUtil from './string_util'
 
 export {
   ArrayUtil,
